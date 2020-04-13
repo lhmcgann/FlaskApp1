d223d2b17a5fb92270af75ce1872041f5db70d5b
lhmcgann <hailey.mcgann@gmail.com>
2020-04-13 13:34:20 -0700

    step 3 - get user by name

--- sample_backend.py
+++ sample_backend.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,8 +8,17 @@ def hello_world():
 
 @app.route('/users')
 def get_users():
+   search_username = request.args.get('name') #accessing val of param 'name'
+   # if there is a name to search for, only return subdict of users w/ name
+   if search_username:
+      subdict = {'users_list' : []} #empty subdict to add to if name match found
+      for user in users['users_list']:
+         if user['name'] == search_username:
+            subdict['users_list'].append(user)
+      return subdict
    return users
 
+# in the form of a JSON object (or python dictionary)
 users = {
    'users_list' :
    [
