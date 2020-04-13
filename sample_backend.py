efae6c815abc371de5dfd9da1465427587d3f0ae
lhmcgann <hailey.mcgann@gmail.com>
2020-04-13 13:57:51 -0700

    step 5: POST method

--- sample_backend.py
+++ sample_backend.py
@@ -1,22 +1,31 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello, World!'
 
-@app.route('/users')
+@app.route('/users', methods=['GET','POST'])
 def get_users():
-   search_username = request.args.get('name') #accessing val of param 'name'
-   # if there is a name to search for, only return subdict of users w/ name
-   if search_username:
-      subdict = {'users_list' : []} #empty subdict to add to if name match found
-      for user in users['users_list']:
-         if user['name'] == search_username:
-            subdict['users_list'].append(user)
-      return subdict
-   return users
+   if request.method == 'GET':
+       search_username = request.args.get('name') #accessing val of param 'name'
+       # if there is a name to search for, only return subdict of users w/ name
+       if search_username:
+          subdict = {'users_list' : []} #empty subdict to add to if name match found
+          for user in users['users_list']:
+             if user['name'] == search_username:
+                subdict['users_list'].append(user)
+          return subdict
+       return users
+   elif request.method == 'POST':
+       userToAdd = request.get_json() #get the data/body of the http request
+       users['users_list'].append(userToAdd)
+       resp = jsonify(success=True) #set the http response to show success
+       #resp.status_code = 200 #optionally, you can always set a response code.
+       # 200 is the default code for a normal response
+       return resp
 
 @app.route('/users/<id>')
 def get_user(id):
