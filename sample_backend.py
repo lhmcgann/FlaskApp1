d1ea47fa8046eb1bf68521d3f83acefe2fc42516
lhmcgann <hailey.mcgann@gmail.com>
2020-04-17 17:44:21 -0700

    Step4.1: return POST status code 201 upon successful creation

--- sample_backend.py
+++ sample_backend.py
@@ -26,7 +26,7 @@ def get_users():
        userToAdd = request.get_json() #get the data/body of the http request
        users['users_list'].append(userToAdd)
        resp = jsonify(success=True) #set the http response to show success
-       #resp.status_code = 200 #optionally, you can always set a response code.
+       resp.status_code = 201 #optionally, you can always set a response code.
        # 200 is the default code for a normal response
        return resp
    elif request.method == 'DELETE':
